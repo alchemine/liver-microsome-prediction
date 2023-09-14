9648a34b135b2b7ab477df44146e94fb0256447c
alchemine <djyoon0223@gmail.com>
2023-09-14 11:35:24 +0900

    [update] Pretraining experiments

--- liver_microsome_prediction/common/config.py
+++ liver_microsome_prediction/common/config.py
@@ -10,6 +10,12 @@ from os.path import abspath, dirname, join
 class PATH:
     root   = dirname(dirname(dirname(abspath(__file__))))
     data   = join(root, "data")
-    train  = join(data, 'train.csv')
-    test   = join(data, 'test.csv')
+    train  = join(data, 'raw/train.csv')
+    test   = join(data, 'raw/test.csv')
     sample = join(data, 'sample_submission.csv')
+    ckpt   = join(root, 'ckpt')
+    out    = join(root, 'out')
+    open   = join(data, 'raw/CID-SMILES')
+
+
+SEED = 42
